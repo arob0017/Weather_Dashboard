62b3fccd1439e273e1bd9e18dcb2a1f62a95f566
Amelia Roberts <arob0017@student.monash.edu>
2020-09-17 12:27:14 +1000

    trying to create function that grabs search input

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 // var searchHistory = JSON.parse(localStorage.getItem("searchArea")) || [];
 
-var city = $("#searchArea")
-    .val()
-    .trim();
-
 function searchForecasts() {
+
+
     $.ajax({ url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",au&appid=c7e4c50860cb5944f39ede1282e773c4" }).then(function (forecastData) {
 
         console.log(forecastData);
         var lon = forecastData.coord.lon;
         var lat = forecastData.coord.lat;
+        function searchWeather(_city_) {
+            $.fetch({ url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",au&appid=c7e4c50860cb5944f39ede1282e773c4" }).then(function (fetchData) {
+                console.log("______")
+            });
+        };
 
         $.ajax({ url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely&units=metric&appid=c7e4c50860cb5944f39ede1282e773c4" }).then(function (allData) {
             console.log(allData)
 
         });
+        searchWeather(_city_);
     });
+
     var cityName = $("<h2>").text(city.name);
     var todayTemp = $("<div>").text(main.temp);
     var feelsLike = $("<div>").text(main.feels_like);
@@ -32,8 +37,10 @@ $("#searchBtn").on("click", function (event) {
     // Preventing the button from trying to submit the form
     event.preventDefault();
     // Storing the city name
-    var searchArea = $("#searchArea").val().trim();
-
-    // Running the searchforecasts function(passing in the search area as an argument)
-    searchForecasts(searchArea);
+    function getSearchVal() {
+        var city = document.querySelector("#search-value").val().trim();
+        //the variable is passed into this function and it will have to then be used in this function
+        searchForecasts(city);
+    }
+    getSearchVal();
 });
